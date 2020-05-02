1a25dd2550ac34cc0334fc68f8095b8acad3e4b4
Jesus Aranda <aranda.jm@icloud.com>
2020-05-02 14:11:48 -0300

    Getting firebase url and apiKey from environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyDQYydhCWkaXDG6Q_UUvLO1EM4xus0smtU',
+  firebaseUrl: 'https://identitytoolkit.googleapis.com/v1/',
 };
 
 /*
