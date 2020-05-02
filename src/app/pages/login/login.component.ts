194147e5195b71a57c76453915bcefdda4a0839c
Jesus Aranda <aranda.jm@icloud.com>
2020-05-02 13:50:03 -0300

    Adding validations into login form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  user: UsuarioModel;
+  constructor() {}
 
   ngOnInit(): void {
+    this.user = new UsuarioModel();
   }
 
+  login(loginForm: NgForm) {
+    if (loginForm.invalid) {
+      return;
+    }
+    console.log(loginForm.value);
+  }
 }
